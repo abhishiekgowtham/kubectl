f3d45fdd35b68c03a515bd0d91bfbe57d162ce26
Jack of All Trades <01jackofalltrades@gmail.com>
2022-11-23 20:06:55 +0800

    add new aws iam auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 version="$1"
 config="$2"
 command="$3"
+aws_iam_auth_version="$4"
 
 if [ "$version" = "latest" ]; then
   version=$(curl -Ls https://dl.k8s.io/release/stable.txt)
@@ -15,6 +16,16 @@ echo "using kubectl@$version"
 curl -sLO "https://dl.k8s.io/release/$version/bin/linux/amd64/kubectl" -o kubectl
 chmod +x kubectl
 mv kubectl /usr/local/bin
+
+curl -sLO "https://dl.k8s.io/release/$version/bin/linux/amd64/kubectl" -o kubectl
+chmod +x kubectl
+mv kubectl /usr/local/bin
+
+echo "using aws-iam-authenticator@$aws_iam_auth_version"
+
+curl -sL -o /usr/local/bin/aws-iam-authenticator "https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v${aws_iam_auth_version}/aws-iam-authenticator_${aws_iam_auth_version}_linux_amd64"
+
+chmod +x /usr/local/bin/aws-iam-authenticator
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$config" | base64 -d > /tmp/config
